7e890a30390c83dab0e9b01cc7b26365b281d579
James LP1 <jameszoryk@msn.com>
2025-09-27 12:24:40 +0100

    basic match day analysis ssm

--- workspace/basic_state_space/runners/07_matchday_1.jl
+++ workspace/basic_state_space/runners/07_matchday_1.jl
@@ -6,15 +6,56 @@ using Statistics, StatsBase, StatsPlots, Distributions
 
 include("/home/james/bet_project/models_julia/workspace/basic_state_space/setup.jl")
 include("/home/james/bet_project/models_julia/workspace/bivariate_maher_model/match_day_utils.jl")
-using .AR1NegativeBinomial
 
 include("/home/james/bet_project/models_julia/workspace/basic_state_space/prediction.jl")
 
-using .AR1NegBiPrediction
+include("/home/james/bet_project/models_julia/workspace/bivariate_maher_model/analysis_funcs.jl")
+using .MatchDayUtils 
 
+using .AR1NegativeBinomial
+using .AR1NegBiPrediction
+using .AR1StateSpace
+using .AR1Prediction
+using .Analysis
+using .AnalysisSSM
 
 all_model_paths = Dict(
-  "ssm_bineg" => "/home/james/bet_project/models_julia/experiments/ar1_poisson_test/ar1_negbi_2425_to_2526_20250926-173118"
+  "ssm_bineg" => "/home/james/bet_project/models_julia/experiments/ar1_poisson_test/ar1_negbi_2425_to_2526_20250926-173118",
+  "ssm_poiss" => "/home/james/bet_project/models_julia/experiments/ar1_poisson_test/ar1_poisson_2425_to_2526_20250926-135921"
 )
+
+
+
+todays_matches = get_todays_matches(["scotland", "england"]; cli_path=CLI_PATH)
+
+patterns_to_exclude = [" U21", r"\bB\b"]
+# Filter out rows where ANY of the patterns are found
+filter!(todays_matches) do row
+    !any(p -> occursin(p, row.event_name), patterns_to_exclude)
+end
+todays_matches
+
+
+odds_df = fetch_all_market_odds(
+    todays_matches,
+    MARKET_LIST;
+    cli_path=CLI_PATH
+)
+
+MatchDayUtils.save_odds_to_csv(odds_df, "data/")
+
+
+loaded_models_all = load_models_from_paths(all_model_paths)
+
+
+match_to_analyze = todays_matches[1, :]
+
+(comparison_df, prediction_matrices, market_book) = MatchDayUtils.generate_match_analysis(
+    match_to_analyze,
+    odds_df, # Your wide DataFrame of all market odds
+    loaded_models_all,
+    MARKET_LIST # Use your comprehensive or specific market list here
+);
+
 
 
