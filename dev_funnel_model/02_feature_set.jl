0acfaad87bdc42c342584ca9055e43f871cdf7b7
James LP1 <jameszoryk@msn.com>
2026-02-10 22:14:13 +0000

    dev funnel model -  extract

--- dev_funnel_model/02_feature_set.jl
+++ dev_funnel_model/02_feature_set.jl
@@ -3,6 +3,7 @@
 using Revise
 using BayesianFootball
 
+pinthreads(:cores)
 ds = Data.load_extra_ds()
 
 
@@ -17,7 +18,7 @@ ds = Data.load_extra_ds()
 cv_config = BayesianFootball.Data.CVConfig(
     # tournament_ids = [56,57],
     tournament_ids = [56],
-    target_seasons = ["22/23"],
+    target_seasons = ["24/25"],
     history_seasons = 0,
     dynamics_col = :match_week,
     # warmup_period = 36,
@@ -56,3 +57,61 @@ feature_sets = BayesianFootball.Features.create_features(
 using Turing
 turing_model = BayesianFootball.Models.PreGame.build_turing_model(funnel_model, feature_sets[1][1])
 chain = sample(turing_model, NUTS(20, 0.65), 10)
+
+
+
+## --- 
+
+using Revise
+using BayesianFootball
+
+using DataFrames
+using Statistics
+using ThreadPinning
+using LinearAlgebra
+pinthreads(:cores)
+BLAS.set_num_threads(1) 
+
+
+ds = Data.load_extra_ds()
+
+cv_config = BayesianFootball.Data.CVConfig(
+    # tournament_ids = [56,57],
+    tournament_ids = [56],
+    target_seasons = ["24/25"],
+    history_seasons = 0,
+    dynamics_col = :match_week,
+    # warmup_period = 36,
+    warmup_period = 36,
+    stop_early = true
+)
+
+
+splits = BayesianFootball.Data.create_data_splits(ds, cv_config)
+train_cfg = BayesianFootball.Training.Independent(parallel=true, max_concurrent_splits=2) 
+sampler_conf = Samplers.NUTSConfig(
+                100,
+                16,
+                50,
+                0.65,
+                10,
+  Samplers.UniformInit(-0.05, 0.05),
+                :perchain,
+)
+
+training_config = Training.TrainingConfig(sampler_conf, train_cfg, nothing, false)
+
+
+funnel_model = BayesianFootball.Models.PreGame.SequentialFunnelModel()
+
+conf_funnel = Experiments.ExperimentConfig(
+                    name = "grw funnel_model",
+                    model = funnel_model,
+                    splitter = cv_config,
+                    training_config = training_config,
+                    save_dir ="./dev_data/"
+)
+
+
+results_funnel = Experiments.run_experiment(ds, conf_funnel)
+
