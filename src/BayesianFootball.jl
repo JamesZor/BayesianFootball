dc5152767d075a029aa8c49c56f1695a1229584d
James LP1 <jameszoryk@msn.com>
2026-03-25 13:40:25 +0000

    evaluation module built wip

--- src/BayesianFootball.jl
+++ src/BayesianFootball.jl
@@ -31,13 +31,15 @@ include("./experiments/experiment-module.jl") #
 include("./predictions/predictions-module.jl") #
 include( "./signals/signals-module.jl")
 
+include("./evaluation/evaluation-module.jl")
+
 include("./synthetic/synthetic-data-module.jl")
 
 include("./backtesting/backtesting-module.jl")
 
 # Export the main modules and key functions/types for users
 # *** UPDATED EXPORTS ***
-export Data, Features, Models, Samplers, Training, Experiments, Predictions, Markets, Calculations, BackTesting
+export Data, Features, Models, Samplers, Training, Experiments, Predictions, Markets, Calculations, BackTesting, Evaluation
 export AbstractFootballModel, Vocabulary, FeatureSet, required_mapping_keys
 
 # Maybe export core config types too?
