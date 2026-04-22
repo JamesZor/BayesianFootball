f1326c1ba6f27f9ebdddd315c96f7fa17c130249
James LP1 <jameszoryk@msn.com>
2026-04-22 16:05:56 +0100

    calibration - module added - bug fixes on imports

--- src/experiments/post_processing.jl
+++ src/experiments/post_processing.jl
@@ -6,7 +6,6 @@ using Base.Threads
 using ..Data
 using ..Features
 using ..Models
-using ..Predictions
 using DataFrames
 using ..TypesInterfaces: AbstractFootballModel
 
@@ -81,17 +80,5 @@ function _latent_state_dict_to_df(raw_preds::Dict)::AbstractDataFrame
 end
 
 
-
-"""
-Function to process an experiment results struct and datastore to get the 
-model inference prediction of the matches ( Out of sample predictions). 
-returns the predictive posterior distribution (PPD) for the 
-specifed markets (has default)
-"""
-function model_inference(ds::Data.DataStore, exp::Experiments.ExperimentResults)::Predictions.PPD 
-  return Predictions.model_inference(
-                Experiments.extract_oos_predictions(ds, exp)
-         )
-end
 
 
