fbc9f9247e9957dc90c81a46122e68ebc39dfa78
James LP1 <jameszoryk@msn.com>
2026-05-24 13:41:20 +0100

    bugfix:  added the missing adapter methods directly into  src/predictions/score_computation/frank_copula.jl

--- src/predictions/score_computation/frank_copula.jl
+++ src/predictions/score_computation/frank_copula.jl
@@ -14,8 +14,8 @@ function compute_score_matrix_discrete_copula(
     S = zeros(Float64, max_goals, max_goals, n_samples)
     
     for k in 1:n_samples
-        dist_h = BayesianFootball.MyDistributions.RobustNegativeBinomial(rh[k], lh[k])
-        dist_a = BayesianFootball.MyDistributions.RobustNegativeBinomial(ra[k], la[k])
+        dist_h = MyDistributions.RobustNegativeBinomial(rh[k], lh[k])
+        dist_a = MyDistributions.RobustNegativeBinomial(ra[k], la[k])
         
         κ_val = kappa[k]
         
@@ -38,10 +38,10 @@ function compute_score_matrix_discrete_copula(
                 v1 = v[j+1]
                 v0 = v[j]
                 
-                C11 = BayesianFootball.MyDistributions.frank_copula(u1, v1, κ_val)
-                C01 = BayesianFootball.MyDistributions.frank_copula(u0, v1, κ_val)
-                C10 = BayesianFootball.MyDistributions.frank_copula(u1, v0, κ_val)
-                C00 = BayesianFootball.MyDistributions.frank_copula(u0, v0, κ_val)
+                C11 = MyDistributions.frank_copula(u1, v1, κ_val)
+                C01 = MyDistributions.frank_copula(u0, v1, κ_val)
+                C10 = MyDistributions.frank_copula(u1, v0, κ_val)
+                C00 = MyDistributions.frank_copula(u0, v0, κ_val)
                 
                 pmf = C11 - C01 - C10 + C00
                 S[i, j, k] = max(pmf, 0.0)
@@ -58,7 +58,7 @@ function compute_score_matrix_discrete_copula(
 end
 
 # 1. Adapter: DataFrame Row -> NamedTuple
-function extract_params(model::BayesianFootball.TypesInterfaces.AbstractFrankCopulaNegBinModel, row)
+function extract_params(model::TypesInterfaces.AbstractFrankCopulaNegBinModel, row)
     return (
         loc_h = row.λ_h,
         loc_a = row.λ_a,
@@ -69,6 +69,6 @@ function extract_params(model::BayesianFootball.TypesInterfaces.AbstractFrankCop
 end
 
 # 2. Kernel Wrapper
-function compute_score_matrix(model::BayesianFootball.TypesInterfaces.AbstractFrankCopulaNegBinModel, params; max_goals::Int=12)
+function compute_score_matrix(model::TypesInterfaces.AbstractFrankCopulaNegBinModel, params; max_goals::Int=12)
     return compute_score_matrix_discrete_copula(params; max_goals=max_goals)
 end
